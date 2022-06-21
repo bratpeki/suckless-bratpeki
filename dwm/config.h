be85d398b73d9584def75d8e05ef686e340c6c5d
bratpeki <katic.peco@gmail.com>
2022-06-21 19:19:12 +0200

    Conversion to regular colorscheme

--- dwm/config.h
+++ dwm/config.h
@@ -4,16 +4,16 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;         /* border pixel of windows */
-static const unsigned int gappx     = 0;         /* gap pixel between windows */
+static const unsigned int gappx     = 32;        /* gap pixel between windows */
 static const unsigned int snap      = 8;         /* snap pixel */
 static const int showbar            = 1;         /* 0 means no bar */
 static const int topbar             = 1;         /* 0 means bottom bar */
 
-static const char col_gray1[]       = "#222c3c";
-static const char col_gray2[]       = "#5d3e2f";
-static const char col_gray3[]       = "#dddddd";
-static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#93634a";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
 
 static const char *fonts[]          = { "Hack Nerd Font Mono:pixelsize=16:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Hack Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
